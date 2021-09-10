c5e48337c1c31628eee659534628836cb8dc3cb5
Luiz Otavio Lima Falcao <luizotavio-falcao@hotmail.com>
2021-09-10 13:27:26 -0300

    Add function to retrieve player selection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const GameBoard = (function () {
-    let _board = ['o', 'x', 'x', 'o', 'o', 'x', 'x', 'o', 'o'];
+    // let _board = ['o', 'x', 'x', 'o', 'o', 'x', 'x', 'o', 'o'];
+    let _board = [];
 
     const getBoard = () => _board;
     const placeMark = (position, mark) => (_board[position] = mark);
@@ -7,13 +8,12 @@ const GameBoard = (function () {
     return { getBoard, placeMark };
 })();
 
-const Player = function () {
-    let _name;
+const Player = function (mark) {
+    let _mark = mark;
 
-    getName = () => _name;
-    setName = (name) => (_name = name);
+    getMark = () => _mark;
 
-    return { getName, setName };
+    return { getMark };
 };
 
 const DisplayController = (function () {
@@ -22,20 +22,45 @@ const DisplayController = (function () {
     getScore = () => _score;
     setScore = (score) => (_score = score);
 
-    return { getScore, setScore };
+    updateBoard = () => {
+        let board = GameBoard.getBoard();
+        for (let i = 0; i <= 8; i++) {
+            if (board[i] === 'x') {
+                const mark = document.querySelector(
+                    `img[data-mark="${i + 1}x"]`
+                );
+                mark.style.display = 'block';
+            } else if (board[i] === 'o') {
+                const mark = document.querySelector(
+                    `img[data-mark="${i + 1}o"]`
+                );
+                mark.style.display = 'block';
+            }
+        }
+    };
+
+    return { getScore, setScore, updateBoard };
 })();
 
-function updateBoard() {
-    let board = GameBoard.getBoard();
-    for (let i = 0; i <= 8; i++) {
-        if (board[i] === 'x') {
-            const mark = document.querySelector(`img[data-mark="${i + 1}x"]`);
-            mark.style.display = 'block';
-        } else if (board[i] === 'o') {
-            const mark = document.querySelector(`img[data-mark="${i + 1}o"]`);
-            mark.style.display = 'block';
-        }
-    }
+let user;
+
+function selectPlayer() {
+    const form = document.querySelector('#player-info');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const mark = document.querySelector('input[type="radio"]:checked');
+        user = new Player(mark.value);
+    });
 }
 
-updateBoard();
+const blocks = document.querySelectorAll('.block');
+blocks.forEach((block) => {
+    block.addEventListener('click', () => {
+        let blockId = block.id.slice(6) - 1;
+        console.log(blockId);
+        GameBoard.placeMark(blockId, user.getMark());
+    });
+});
+
+selectPlayer();
+DisplayController.updateBoard();
