f0e441101a5ea7e56a86f89a363c8c0254e39ce8
Luiz Otávio Lima Falcão <luizotavio-falcao@hotmail.com>
2021-09-10 22:29:40 -0300

    Refactor board functions

--- index.js
+++ index.js
@@ -1,47 +1,25 @@
 const GameBoard = (function () {
     let _board = [];
-
     const getBoard = () => _board;
     const placeMark = (position, mark) => (_board[position] = mark);
-
-    return { getBoard, placeMark };
-})();
-
-const Player = function (mark) {
-    let _mark = mark || 'X';
-
-    getMark = () => _mark;
-    setMark = (mark) => (_mark = mark);
-
-    return { getMark, setMark };
-};
-
-const DisplayController = (function () {
-    let _score = [0, 0];
-
-    getScore = () => _score;
-    setScore = (score) => (_score = score);
 
     markBoard = (player) => {
         const blocks = document.querySelectorAll('.block');
         blocks.forEach((block) => {
             block.addEventListener('click', () => {
                 let blockId = block.id.slice(6) - 1;
-
                 if (block.classList.contains('active')) {
                     return;
                 }
-
                 GameBoard.placeMark(blockId, player.getMark());
                 block.classList.add('active');
-                DisplayController.updateBoard();
+                updateBoard();
             });
         });
     };
 
     updateBoard = () => {
         let board = GameBoard.getBoard();
-
         for (let i = 0; i <= 8; i++) {
             if (board[i] === 'X') {
                 const mark = document.querySelector(
@@ -57,16 +35,38 @@ const DisplayController = (function () {
         }
     };
 
-    return { getScore, setScore, markBoard, updateBoard };
+    return { getBoard, placeMark, markBoard, updateBoard };
 })();
 
-function selectMark(player) {
+const Player = function (mark) {
+    let _mark = mark || 'X';
+    const getMark = () => _mark;
+    const setMark = (mark) => (_mark = mark);
+
+    return { getMark, setMark };
+};
+
+const DisplayController = (function () {
+    let _score = [0, 0];
+    let _isAITurn = false;
+    const getScore = () => _score;
+    const setScore = (score) => (_score = score);
+    const getIsAITurn = () => _isAITurn;
+
+    const startGame = () => {
+        GameBoard.markBoard(user);
+    };
+
+    return { getScore, setScore, startGame, getIsAITurn };
+})();
+
+function selectPlayer(player) {
     const mark = document.querySelector('input[type="radio"]:checked');
     player.setMark(mark.value);
 }
 
 const submitBtn = document.querySelector('#player-info button');
 let user = new Player();
-submitBtn.addEventListener('click', selectMark(user));
+submitBtn.addEventListener('click', selectPlayer(user));
 
-DisplayController.markBoard(user);
+DisplayController.startGame();
